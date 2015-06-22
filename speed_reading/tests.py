9c023dfe8b18cb1f8fbb0205d77208ed364bec25
Brendan <bwallace722@gmail.com>
2015-06-22 14:41:02 -0400

    deleted the changes from the example branches

--- speed_reading/tests.py
+++ speed_reading/tests.py
@@ -1,8 +1,5 @@
 from django.test import TestCase
 
-#this is an unnecessary comment
 class AttemptModelTests(TestCase):
     def test_words_per_minute(self):
         self.AssertTrue(True)
-    def test_true(self):
-	self.AssertTrue(False)
